7737d42e47c4b8044a3223af491e41e58c7e8e71
Saleh Almeraj <saleh.mearaj@gmail.com>
2019-11-27 00:59:08 +0300

    add openweathermap api function

--- web/functions/openweather/openweather.js
+++ web/functions/openweather/openweather.js
@@ -1,10 +1,14 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const fetch = require('node-fetch')
+const apiKey = process.env.OPENWEATHER_API_KEY
+const url = `http://api.openweathermap.org/data/2.5/forecast?id=110107&APPID=${apiKey}`
 exports.handler = async (event, context) => {
   try {
     const subject = event.queryStringParameters.name || 'World'
+    const result = await fetch(url).then(res => res.json())
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` })
+      body: JSON.stringify({ ...result })
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
